9ddf983000aadcf9f1cdcf6c834c5e2bf4991376
ikepon <tatsuyanoheya@gmail.com>
2016-05-25 15:06:04 +0900

    mirage を使ってモデルを定義 #1

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,21 +1,7 @@
 import Ember from 'ember';
 
-var notes = [{
-  id: 1,
-  title: 'NoteTitle 1',
-  body: 'note body1 note body1 note body1 note body1 note body1 note body1 note body1 note body1 note body1 note body1 note body1 note body1 note body1'
-}, {
-  id: 2,
-  title: 'NoteTitle 2',
-  body: 'note body2 note body2 note body2 note body2 note body2 note body2 note body2 note body2 note body2 note body2 note body2 note body2 note body2'
-}, {
-  id: 3,
-  title: 'NoteTitle 3',
-  body: 'note body3 note body3 note body3 note body3 note body3 note body3 note body3 note body3 note body3 note body3 note body3 note body3 note body3'
-}];
-
 export default Ember.Route.extend({
   model() {
-    return notes;
+    return this.store.findAll('note');
   }
 });
