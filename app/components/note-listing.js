89f36b3870c3685ec0097cb7d42e3b03b601a55b
ikepon <tatsuyanoheya@gmail.com>
2016-05-26 18:37:54 +0900

    missing semicolon を修正 #1

--- app/components/note-listing.js
+++ app/components/note-listing.js
@@ -3,7 +3,7 @@ import Ember from 'ember';
 export default Ember.Component.extend({
   actions: {
     deleteNote(note) {
-      this.sendAction('deleteNote', note)
+      this.sendAction('deleteNote', note);
     }
   }
 });
