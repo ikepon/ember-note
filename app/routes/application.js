ace6a10dd6a1ee37a3500b15df0bf8a6b6cbd035
ikepon <tatsuyanoheya@gmail.com>
2016-05-27 13:27:15 +0900

    route が変わった時のログをとる（将来的にGAなどに使う） #1

--- app/routes/application.js
+++ app/routes/application.js
@@ -1,6 +1,13 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  didUrlChange: Ember.observer('router.url', function() {
+    Ember.run.next(() => {
+      let currentPath = this.get('router.url');
+      console.log(currentPath);
+    })
+  }),
+
   model() {
     return this.store.findAll('note');
   },
